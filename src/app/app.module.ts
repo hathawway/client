68fb93dae988bd7f7461616c657349031848f46a
Мейер Оля <mkim95165@gmail.com>
2022-01-08 13:06:40 +0300

    add all start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayotComponent } from './app/components/layot/layot.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { PostTableComponent } from './components/post-table/post-table.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayotComponent,
+    SidebarComponent,
+    LayoutComponent,
+    PostTableComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
