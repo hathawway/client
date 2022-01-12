f32e215d1f3bfb05ebae262e94a4b3eb4ad7e114
Olya Meyer <hathawway@gmail.com>
2022-01-12 13:38:52 +0300

    настройка роутеров в sidebar

--- src/app/components/shared/sidebar/sidebar.component.ts
+++ src/app/components/shared/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
@@ -7,7 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidebarComponent implements OnInit {
 
-  constructor() { }
+
+  links = [  
+    {url: '/post', name: 'Структурные подразделения'},
+    {url: '/office', name: 'Должности'},
+    {url: '/user', name: 'Пользователи'}
+  ]
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
