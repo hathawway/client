e43d32b471f10e154f58e2df7c2d0fc048ff4219
Olya Meyer <hathawway@gmail.com>
2022-02-10 13:15:16 +0400

    форма office

--- src/app/components/admin/modal-add-office-table/modal-add-office-table.component.ts
+++ src/app/components/admin/modal-add-office-table/modal-add-office-table.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, HostBinding, HostListener, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
 import { of, switchMap } from 'rxjs';
 import { MaterialService } from 'src/app/classes/material.service';
@@ -16,8 +16,10 @@ export class ModalAddOfficeTableComponent implements OnInit {
   @HostBinding("style.visibility") visibility = "hidden"
   @Input() @HostBinding("style.width") width = "600px"
  
-  name!: String;
-  form!: FormGroup;
+  //name!: string;
+  postForm!: FormGroup;
+  of !: BookOffice ;
+
 
   constructor(private officeService : OfficeService,
     private route: ActivatedRoute) { }
@@ -30,10 +32,12 @@ export class ModalAddOfficeTableComponent implements OnInit {
 
  
     this.visibility = "visible"
-    console.log('jedj')
-    /*this.form = new FormGroup({
-      name: new FormControl(null, Validators.required)
+    this.postForm = new FormGroup({
+      id: new FormControl(office.id, Validators.required),
+      name: new FormControl(office.name, Validators.required)
     })
+
+    //console.log(params['id'])
 
     /*this.route.params.pipe(switchMap( (params: Params) => {
         if (params['id']) {
@@ -49,28 +53,26 @@ export class ModalAddOfficeTableComponent implements OnInit {
         }
       }, error => MaterialService.toast(error.error.message)
       )*/
+      /*this.postForm.valueChanges.subscribe((v) => {
+        console.log(v)
+      
+        
+      })*/
+
+
+
     e.stopPropagation()
-    console.log('j111111edj')
     
   }
  
   close() {
     this.visibility = "hidden"
   }
- 
-  save() {
-    const office = {
-      name: this.name
-    }
-    if (this.officeService.checkName(office.name)) {     
-      //this.officeService.addOffice(office)
-      this.visibility = "hidden"
-    }
-    
-  }
 
   onSubmit() {
-
+    this.officeService.updateOffice(this.postForm.value)
+    console.log(this.postForm.value)
+    this.visibility = "hidden"
   }
 
 }
